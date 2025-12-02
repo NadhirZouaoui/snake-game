463566fa26d8648fc401e68ec09e95e93c4403dc
NadhirZouaoui <nadhirzouaoui256@gmail.com>
2025-12-02 20:58:58 +0100

    refactor(App): move Vector2 above Snake, make direction enum for clarity

--- Snake_Game_App/Source/App.cpp
+++ Snake_Game_App/Source/App.cpp
@@ -1,15 +1,5 @@
 #include "Core/Core.h"
-class Snake
-{
-private:
-	int speed, direction, length;
-	Vector2 headPosition;
-	std::vector<int> body;
 
-
-public:
-	
-};
 
 class Vector2
 {
@@ -17,8 +7,30 @@ public:
 	int x, y;
 	bool operator==(auto& other) {
 		return this.x == other.x && this.y == other.y;
- 	}
-	Vector2() : x(0), y(0){};
+	}
+	Vector2() : x(0), y(0) {};
+};
+
+enum Direction
+{
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT
+};
+
+
+class Snake
+{
+private:
+	int m_speed, m_length;
+	Direction m_direction;
+	Vector2 m_headPosition;
+	std::vector<Vector2> m_bodyPosition;
+
+
+public:
+
 };
 
 
